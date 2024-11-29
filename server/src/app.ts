d61da6ee8db47c96d112a80da1d2f6b19b09a5a1
dariykutelov <dariy@kutelov.com>
2024-11-29 16:53:15 +0200

    removed node_modules and .env

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import { api } from "./routes/api";
 import "./db";
 const app = express();
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.use(morgan("combined"));
 app.use(cors());
